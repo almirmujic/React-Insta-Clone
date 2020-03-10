27c54b068f0460bb457193b26133544dc8362b4b
Almir <contact.almir@gmail.com>
2020-03-09 19:56:48 -0500

    Initial commit - clean create-react-app defaults

---
+++ instagram/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer';
+
+export default function App() {
+	return (
+		<div>
+			<SearchBar />
+			<PostContainer />
+		</div>
+	);
+}
