2c2a801b94dd63b940521a57de210dd89f8d2da8
Almir <contact.almir@gmail.com>
2020-03-09 20:16:18 -0500

    Import dummy-data - map out each array item as a PostContainer component

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import SearchBar from './components/SearchBar/SearchBar';
 import PostContainer from './components/PostContainer/PostContainer';
 
+import dummyData from './dummy-data';
+
 export default function App() {
 	return (
 		<div>
 			<SearchBar />
-			<PostContainer />
+			{dummyData.map((post) => <PostContainer />)}
 		</div>
 	);
 }
