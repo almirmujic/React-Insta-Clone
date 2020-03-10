ea72bdcc2e29862e1448dd08c2f2a2423f2a6429
Almir <contact.almir@gmail.com>
2020-03-09 19:57:12 -0500

    Initial commit

---
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CommentSection from '../CommentSection/CommentSection';
+
+export default function PostContainer() {
+	return (
+		<div>
+			Post container
+			<CommentSection />
+		</div>
+	);
+}
