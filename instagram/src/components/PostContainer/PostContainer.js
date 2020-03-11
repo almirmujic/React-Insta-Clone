f144f20a3d6724f2081a14ab6bca12baa0fc8eea
Almir <contact.almir@gmail.com>
2020-03-10 20:34:44 -0500

    Add PostContainer styling

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -6,8 +6,8 @@ import './PostContainer.css';
 
 export default function PostContainer(props) {
 	return (
-		<div>
-			<header>
+		<div className="post-container">
+			<header className="post-header">
 				<img
 					className="thumbnail"
 					alt={props.post.username}
@@ -15,9 +15,37 @@ export default function PostContainer(props) {
 				/>
 				<span>{props.post.username}</span>
 			</header>
+			<img
+				className="post-image"
+				src={props.post.imageUrl}
+				alt={props.post.username}
+			/>
+			<div className="post__details">
+				<div className="post__content">
+					<div className="like-comment">
+						<img
+							className="lc-icon"
+							src="https://i.ya-webdesign.com/images/heart-symbol-png-19.png"
+							alt="like"
+						/>
+						<img
+							className="lc-icon2"
+							src="https://cdn0.iconfinder.com/data/icons/instagram-ui-1/24/Instagram-UI_comment-512.png"
+							alt="comment"
+						/>
+					</div>
+					<span style={{ fontWeight: '600' }}>
+						{props.post.likes} likes
+					</span>
+					<CommentSection comments={props.post.comments} />
+				</div>
 
-			<CommentSection comments={props.post.comments} />
-			<input type="text" placeholder="Make a comment..." />
+				<input
+					className="add-comment"
+					type="text"
+					placeholder="Add a comment..."
+				/>
+			</div>
 		</div>
 	);
 }
