d349e11a7f885c474e3d1b4712cb0a417d96f9ba
Almir <contact.almir@gmail.com>
2020-03-10 21:33:52 -0500

    Set data as state and pass it down into mapped out PostContainers

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './components/SearchBar/SearchBar';
 import PostContainer from './components/PostContainer/PostContainer';
 
@@ -7,11 +7,17 @@ import dummyData from './dummy-data';
 import './App.css';
 
 export default function App() {
+	const [ data, setData ] = useState([]);
+
+	useEffect(() => {
+		setData(dummyData);
+	}, []);
+
 	return (
 		<div style={{ maxWidth: '1000px', margin: '0 auto' }}>
 			<SearchBar />
 			<div className="post-layout">
-				{dummyData.map((post) => (
+				{data.map((post) => (
 					<PostContainer key={post.id} post={post} />
 				))}
 			</div>
