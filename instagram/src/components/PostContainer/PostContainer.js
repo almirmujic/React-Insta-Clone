c91c9f2567f18aede56b06e0ed2410766994c25d
Almir <contact.almir@gmail.com>
2020-03-13 14:35:35 -0500

    Implement like post functionality

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import CommentSection from '../CommentSection/CommentSection';
+
+import Heart from '../../assets/Heart';
 
 import './PostContainer.css';
 
 export default function PostContainer(props) {
+	const [ likes, setLikes ] = useState(props.post.likes);
+	const [ liked, setLiked ] = useState(false);
+
+	useEffect(
+		() => {
+			setLikes(
+				(prevState) => (liked ? prevState + 1 : prevState - 1)
+			);
+		},
+		[ liked ]
+	);
+
+	const toggleLike = () => {
+		setLiked((prevState) => !prevState);
+	};
 	return (
 		<div className="post-container">
 			<header className="post-header">
@@ -23,28 +40,18 @@ export default function PostContainer(props) {
 			<div className="post__details">
 				<div className="post__content">
 					<div className="like-comment">
-						<img
-							className="lc-icon"
-							src="https://i.ya-webdesign.com/images/heart-symbol-png-19.png"
-							alt="like"
-						/>
-						<img
-							className="lc-icon2"
-							src="https://cdn0.iconfinder.com/data/icons/instagram-ui-1/24/Instagram-UI_comment-512.png"
-							alt="comment"
+						<Heart
+							customClass="lc-icon"
+							alt="heart"
+							color={liked ? 'red' : '#333'}
+							toggleLike={toggleLike}
 						/>
 					</div>
 					<span style={{ fontWeight: '600' }}>
-						{props.post.likes} likes
+						{likes} likes
 					</span>
 					<CommentSection comments={props.post.comments} />
 				</div>
-
-				<input
-					className="add-comment"
-					type="text"
-					placeholder="Add a comment..."
-				/>
 			</div>
 		</div>
 	);
