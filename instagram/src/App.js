e42fe81a6c4146642c1f8a614fe8bd1b5456d4b1
Almir <contact.almir@gmail.com>
2020-03-23 13:49:52 -0500

    Wrap PostsPage with hoc

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+import PostsPage from './components/PostsPage/PostsPage';
+
+import withAuthenticate from './components/authentication/withAuthenticate';
+
 import './App.css';
 
-export default function App() {}
+export default function App() {
+	const ComponentFromWithAuthenticate = withAuthenticate(PostsPage);
+
+	return <ComponentFromWithAuthenticate />;
+}
